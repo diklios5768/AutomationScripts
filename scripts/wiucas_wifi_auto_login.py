9228658c024a697888d0fe7e6fa1fa15ebc9cd78
diklios <47837462+diklios5768@users.noreply.github.com>
2022-09-07 17:58:10 +0800

    增加国科大温州研究院WiFi自动登录代码（需要输入用户名和密码）

---
+++ scripts/wiucas_wifi_auto_login.py
@@ -0,0 +1,50 @@
+# -*- encoding: utf-8 -*-
+"""
+@File Name      :   wiucas_wifi_auto_login.py
+@Create Time    :   2022/9/7 17:57
+@Description    :   
+@Version        :   
+@License        :   MIT
+@Author         :   diklios
+@Contact Email  :   diklios5768@gmail.com
+@Github         :   https://github.com/diklios5768
+@Blog           :   
+@Motto          :   All our science, measured against reality, is primitive and childlike - and yet it is the most precious thing we have.
+"""
+__auth__ = 'diklios'
+
+import click
+# 请测试完毕后删除所有代码
+import requests
+
+base_url = 'http://192.168.200.251:8008'
+login_url = f'{base_url}/portal.cgi'
+login_page_url = f'{base_url}/user_auth_verify.cgi'
+
+
+@click.command()
+@click.argument('username', type=str)
+@click.argument('password', type=str)
+def main(username, password):
+    login_page = requests.post(login_page_url, data={
+        'submit': 'submit'
+    })
+    print(login_page.text)
+    code = login_page.json()['code']
+    print(f'code:{code}')
+    data = {
+        'username': username,
+        'password': password,
+        'uplcyid': '1',
+        'language': '0',
+        'code': code,
+        'submit': 'submit'
+    }
+    print(f'data:{data}')
+    login = requests.post(login_url, data=data)
+    login.encoding = 'utf-8'
+    print(login.text)
+
+
+if __name__ == '__main__':
+    main()
